f79ca3537e94e182c6871c0b58026123a5127b02
bryan-herrera <herrerabryan1@gmail.com>
2021-07-16 17:46:29 -0500

    encapsulamiento componente Empleado

--- src/app/pages/employees/edit/edit.component.ts
+++ src/app/pages/employees/edit/edit.component.ts
@@ -1,10 +1,6 @@
-
 import { Component } from '@angular/core';
 
-
 @Component({
-  selector: 'app-edit',
   template: `<app-employee-form></app-employee-form>`,
-  styleUrls: ['./edit.component.scss']
 })
 export class EditComponent {}
--- src/app/pages/employees/new/new.component.ts
+++ src/app/pages/employees/new/new.component.ts
@@ -1,15 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-new',
+
   template: `<app-employee-form></app-employee-form>`,
-  styleUrls: ['./new.component.scss']
+
 })
-export class NewComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+export class NewComponent {
 }
